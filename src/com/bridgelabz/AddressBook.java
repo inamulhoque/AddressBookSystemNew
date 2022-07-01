189e813aa3df868ebbf10139619a748132ed9439
Inamul <inamulhoque097@gmail.com>
2022-07-01 23:30:42 +0530

    Added: Added new contact details

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -10,27 +10,30 @@ public class AddressBook {
     static int zip;
     static long phone;
     static String mail;
+    static int i = 1;
 
     public void addressBook() {
         Scanner obj = new Scanner(System.in);
+        do {
+            System.out.print("First name: ");
+            firstName = obj.next();
+            System.out.print("Last name: ");
+            lastName = obj.next();
+            System.out.print("City: ");
+            city = obj.next();
+            System.out.print("State: ");
+            state = obj.next();
+            System.out.print("Zip: ");
+            zip = obj.nextInt();
+            System.out.print("E-mail: ");
+            mail = obj.next();
+            System.out.print("Phone number: ");
+            phone = obj.nextLong();
 
-        System.out.print("First name: ");
-        firstName=obj.next();
-        System.out.print("Last name: ");
-        lastName=obj.next();
-        System.out.print("City: ");
-        city=obj.next();
-        System.out.print("State: ");
-        state=obj.next();
-        System.out.print("Zip: ");
-        zip=obj.nextInt();
-        System.out.print("E-mail: ");
-        mail=obj.next();
-        System.out.print("Phone number: ");
-        phone=obj.nextLong();
-    }
-    public void display() {
-        System.out.println("First name:"+firstName+"\tLast name:"+lastName+"\tCity:"+city+"\tState:"+state+"\tZip:"+zip+
-                "\tPhone:"+phone+"\tE-mail:"+mail);
+            System.out.println("First name:" + firstName + "\tLast name:" + lastName + "\tCity:" + city + "\tState:" + state + "\tZip:" + zip +
+                    "\tPhone:" + phone + "\tE-mail:" + mail);
+            System.out.println("Do you want to add more? Press 1 or you can exit with any input exceept 1.");
+            i = obj.nextByte();
+        } while (i == 1);
     }
 }
--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -1,10 +1,9 @@
 package com.bridgelabz;
 
-public class Main {
+public class Main extends AddressBook{
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program on Master Branch");
         AddressBook obj = new AddressBook();
         obj.addressBook();
-        obj.display();
     }
 }
