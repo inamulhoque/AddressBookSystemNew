bb78d508a776bcb26fb5abe08bf7758f1210a63c
Inamul <inamulhoque097@gmail.com>
2022-07-01 23:58:16 +0530

    Added: Edit contact details of addressbook

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -1,9 +1,11 @@
 package com.bridgelabz;
 
-public class Main extends AddressBook{
+public class Main{
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program on Master Branch");
         AddressBook obj = new AddressBook();
         obj.addressBook();
+        EditAddressBook obj2 = new EditAddressBook();
+        obj2.editAddressBook();
     }
 }
