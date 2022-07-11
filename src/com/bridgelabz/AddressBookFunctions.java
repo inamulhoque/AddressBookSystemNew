bde6526cb56e9eae5d996d01577a2c22fd038ff2
Inamul <inamulhoque097@gmail.com>
2022-07-11 09:47:30 +0530

    Added: checking if any duplicate name is there.

--- src/com/bridgelabz/AddressBookFunctions.java
+++ src/com/bridgelabz/AddressBookFunctions.java
@@ -8,36 +8,19 @@ import java.util.Scanner;
 import static com.bridgelabz.AddressBook.*;
 
 public class AddressBookFunctions {
-        public static ArrayList<AddressBook> arrayOfContacts = new ArrayList<>();
-        public static Scanner obj = new Scanner(System.in);
-        public static HashMap<String,ArrayList<AddressBook>> hashMapOfAddressBook = new HashMap<>();
+    public static ArrayList<AddressBook> Contacts = new ArrayList<>();
+    public static Scanner obj = new Scanner(System.in);
+    public static HashMap<String,ArrayList<AddressBook>> hashMapOfAddressBook = new HashMap<>();
 
-        public static ArrayList<AddressBook> findAddressBook(String name){
-            for (Map.Entry<String,ArrayList<AddressBook>> itr: hashMapOfAddressBook.entrySet()){
-                if (itr.getKey().equals(name)){
-                    return itr.getValue();
-                }
-            }
-            return null;
-        }
-        public static String addAddressBook(){
-            System.out.println("Enter addressbook name:");
-            String addressBookName = obj.next();
-            if (findAddressBook(addressBookName)!=null){
-                System.out.println("Already exists.");
-                System.out.println(hashMapOfAddressBook.get(addressBookName));
-            }
-            return addressBookName;
-        }
+    public static void addContact(String bookName){
+        System.out.print("First name: ");
+        firstName = obj.next();
+        AddressBook.setFirstName(firstName);
 
-        public static void addContact(String bookName){
-            System.out.print("First name: ");
-            firstName = obj.next();
-            AddressBook.setFirstName(firstName);
-
-            System.out.print("Last name: ");
-            lastName = obj.next();
-            AddressBook.setLastName(lastName);
+        System.out.print("Last name: ");
+        lastName = obj.next();
+        AddressBook.setLastName(lastName);
+        if (AddressBookFunctions.checkDuplicate(bookName,firstName,lastName)) {
 
             System.out.print("City: ");
             city = obj.next();
@@ -59,14 +42,19 @@ public class AddressBookFunctions {
             phone = obj.nextLong();
             AddressBook.setPhone(phone);
 
-            AddressBook addressBook = new AddressBook(firstName,lastName,city,state,zip,phone,mail);
-            if (findAddressBook(bookName)!=null)
+            AddressBook addressBook = new AddressBook(firstName, lastName, city, state, zip, phone, mail);
+            if (findAddressBook(bookName) != null)
                 hashMapOfAddressBook.get(bookName).add(addressBook);
 
-            arrayOfContacts = new ArrayList<AddressBook>();
-            arrayOfContacts.add(addressBook);
-            hashMapOfAddressBook.put(bookName,arrayOfContacts);
+            Contacts = new ArrayList<AddressBook>();
+            Contacts.add(addressBook);
+            hashMapOfAddressBook.put(bookName, Contacts);
         }
+        else {
+            System.out.println("The contact name with "+firstName+" already exist in "+hashMapOfAddressBook.get(bookName));
+        }
+    }
+
     public static void editContact() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the first name to edit contact details.");
@@ -88,15 +76,47 @@ public class AddressBookFunctions {
             mail = scanner.next();
         }
     }
-    public static void deleteContact(ArrayList<AddressBook> contacts){
+
+    public static void deleteContact(ArrayList<AddressBook> Contacts){
         System.out.println("Enter the first name to delete contact");
         Scanner scanner = new Scanner(System.in);
         String name = scanner.next();
         if (name.equals(firstName)){
-            contacts.removeIf(addressBook -> addressBook.firstName.equals(name));
+            Contacts.clear();
             System.out.println("Deletion completed.");
         } else {
             System.out.println("Wrong name input.");
         }
+    }
+
+    public static void addAddressBook(){
+        System.out.println("Enter addressbook name:");
+        String addressBookName = obj.next();
+        if (findAddressBook(addressBookName)!=null){
+            System.out.println("Already exists.");
+            System.out.println(hashMapOfAddressBook.get(addressBookName));
+        }
+        System.out.println( addressBookName);
+    }
+
+    public static ArrayList<AddressBook> findAddressBook(String name){
+        for (Map.Entry<String,ArrayList<AddressBook>> itr: hashMapOfAddressBook.entrySet()){
+            if (itr.getKey().equals(name)){
+                return itr.getValue();
+            }
+        }
+        return null;
+    }
+
+    public static boolean checkDuplicate(String book_name, String first_name, String last_name){
+        int f = 0;
+        if (hashMapOfAddressBook.get(book_name)==null)
+            return true;
+        ArrayList<AddressBook> arrayList = hashMapOfAddressBook.get(book_name);
+        for (AddressBook data : arrayList) {
+            if (data.getFirstName().equals(first_name) && data.getLastName().equals(last_name))
+                return false;
+        }
+        return true;
     }
 }
--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -3,39 +3,45 @@ package com.bridgelabz;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import static com.bridgelabz.AddressBookFunctions.arrayOfContacts;
+import static com.bridgelabz.AddressBookFunctions.Contacts;
 
 public class AddressBookMain{
 
     public static void main(String[] args) {
-        System.out.println("Enter your choice from below:");
-        System.out.println("1.Add AddressBook"+
-                "2.Add contact\t" +
-                "3.Edit contacts\t" +
-                "4.Delete contacts\t");
-        Scanner scanner = new Scanner(System.in);
-        int input  = scanner.nextInt();
-        switch (input) {
-            case 1:
-                AddressBookFunctions.addAddressBook();
-            case 2:
-                int i =1;
-                do {
-                    System.out.println("Enter addressbook name:");
-                    String book = scanner.next();
-                    AddressBookFunctions.addContact(book);
-                    System.out.println("Do you want to add more contact? press 1 or press any key to exit.");
-                    i = scanner.nextInt();
-                } while (i==1);
-                break;
-            case 3:
-                AddressBookFunctions.editContact();
-                break;
-            case 4:
-                AddressBookFunctions.deleteContact((ArrayList<AddressBook>) arrayOfContacts);
-                break;
-            default:
-                System.out.println("Enter a valid choice.");
-        }
+        int loop = 1;
+        do {
+            System.out.println("Enter your choice from below:");
+            System.out.println("1.Add AddressBook" +
+                    "2.Add contact\t" +
+                    "3.Edit contacts\t" +
+                    "4.Delete contacts\t");
+            Scanner scanner = new Scanner(System.in);
+            int input = scanner.nextInt();
+            switch (input) {
+                case 1:
+                    AddressBookFunctions.addAddressBook();
+                case 2:
+                    int i = 1;
+                    do {
+                        System.out.println("Enter addressbook name:");
+                        String book = scanner.next();
+                        AddressBookFunctions.addContact(book);
+                        System.out.println("Do you want to add more contact? press 1 or press any key to exit.");
+                        i = scanner.nextInt();
+                    } while (i == 1);
+                    break;
+                case 3:
+                    AddressBookFunctions.editContact();
+                    break;
+                case 4:
+                    AddressBookFunctions.deleteContact((ArrayList<AddressBook>) Contacts);
+                    break;
+                default:
+                    System.out.println("Enter a valid choice.");
+                    break;
+            }
+            System.out.println("Press 1 to repeat the menu or quit.");
+            loop = scanner.nextInt();
+        }while (loop==1);
     }
 }
